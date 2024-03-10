f2973c8a83073988f85df4d13dab625ad66541b9
nice-0 <twe@luna>
2024-03-11 01:45:43 +1300

    a

--- bin/initial_install.sh
+++ bin/initial_install.sh
@@ -45,7 +45,7 @@ esac
 
 pushd /tmp
 nix-shell -p git --run "git clone https://github.com/nice-0/copycat.git"
-nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/copycat/${PHASE}/disko.nix --arg device '"/dev/'${DISK_DEV}'"'
+nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/copycat/"${PHASE}"/disko.nix --arg device '"/dev/'${DISK_DEV}'"'
 
 nixos-generate-config --no-filesystems --root /mnt
 
