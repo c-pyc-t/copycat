3af91cdcec7d3abbe78e0a4751842302621053e7
nice-0 <twe@luna>
2024-03-11 12:45:34 +1300

    missed some obv shit

--- bin/update.sh
+++ bin/update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# this needs to be done better ...
-
-
-cd /etc/nixos/
-git pull origin main
-nixos-reload --flake /etc/nixos#copycat switch
+# need to think on this one
+#cd /etc/nixos/
+#git pull origin main
+#nixos-reload --flake /etc/nixos#copycat switch
 
