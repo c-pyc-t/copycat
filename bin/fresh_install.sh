1b2a2344f0e3bdfed9e0c66969a92c05e8b88881
c-pyc-t <copycat@imp.nz>
2024-04-12 19:38:39 +1200

    quickly iterating to get shit up and running

--- bin/fresh_install.sh
+++ bin/fresh_install.sh
@@ -18,20 +18,16 @@
 set -e
 
 alias psak=$(read -t 5 -n 1 -s -r -p "Press any key to continue")
-alias cc=$(
-echo "
-                                              ╻
-      ┏━━━┓ ┏━━━┓ ┏━━━┓ ╻   ╻ ┏━━━┓ ┏━━━┓ ━━━━┫
-  d   ┃           ┃   ┃ ┃   ┃ ┃               ┃
-  r   ┃     ━━━━━ ┃   ┃ ┃   ┃ ┃     ━━━━━     ┃
-  g @ ┃           ┃   ┃ ┃   ┃ ┃               ┃
-  n   ┗━━━┛ ┗━━━┛ ┣━━━┛ ┗━━━┫ ┗━━━┛ ┗━━─┦     ╹
-                  ┃         ┃
-                  ┃         ┃               24.
-"
-)
+echo "                                              ╻"
+echo "      ┏━━━┓ ┏━━━┓ ┏━━━┓ ╻   ╻ ┏━━━┓ ┏━━━┓ ━━━━┫"
+echo "  d   ┃           ┃   ┃ ┃   ┃ ┃               ┃"
+echo "  r   ┃     ━━━━━ ┃   ┃ ┃   ┃ ┃     ━━━━━     ┃"
+echo "  g @ ┃           ┃   ┃ ┃   ┃ ┃               ┃"
+echo "  n   ┗━━━┛ ┗━━━┛ ┣━━━┛ ┗━━━┫ ┗━━━┛ ┗━━─┦     ╹"
+echo "                  ┃         ┃									"
+echo "                  ┃         ┃               24."
+
 clear 
-cc
 psak
 #echo "Do you wish to install this program?"
 #select yn in "Yes" "No"; do
