a8de15a3c560aab68d61ed2e7d0e2df33ef00387
c-pyc-t <copycat@imp.nz>
2024-04-27 04:05:26 +1200

    2024-04-27 04:05:26.725339897+12:00 echo hi

--- scripts/copycat_update.sh
+++ scripts/copycat_update.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 pushd /copycat 1 2>/dev/null
-sudo git config --add safe.directory /copycat
+# sudo git config --add safe.directory /copycat
+git config --global --add safe.directory /copycat
+git remote set-url origin git@github.com:c-pyc-t/copycat.git
 git config user.name c-pyc-t
 git config user.email copycat@imp.nz
-git config core.sshCommand "ssh -i /copycat/keys/sys/ssh_system.key"
+git config core.sshCommand "ssh -i /static/console/keys/sys/ssh_system.key"
 
 TIMESTAMP=$(date --rfc-3339=ns)
 
-sudo -u copycat git add -A 
-sudo -u copycat git commit -F- <<EOF
+git add -A 
+git commit -F- <<EOF
 $TIMESTAMP
 echo hi
 EOF
