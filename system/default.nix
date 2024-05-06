3cff6b7bb0085f07fb501adc41c4c8c2607d5e5a
c-pyc-t <copycat@imp.nz>
2024-05-07 00:49:42 +1200

    copycat@lapcat [pass] [2024-05-07 00:48:24.726671978+12:00]
    Generation  Build-date           NixOS version           Kernel      Configuration Revision  Specialisation
    64 current  2024-05-07 00:49:40  24.05.20240423.572af61  6.8.6-zen1                          *
    63          2024-05-07 00:47:44  24.05.20240423.572af61  6.8.6-zen1                          *
    62          2024-05-07 00:45:48  24.05.20240423.572af61  6.8.6-zen1                          *
    61          2024-05-07 00:13:35  24.05.20240423.572af61  6.8.6-zen1                          *
    60          2024-05-07 00:10:35  24.05.20240423.572af61  6.8.6-zen1                          *
    59          2024-05-05 20:05:53  24.05.20240423.572af61  6.8.6-zen1                          *
    58          2024-05-04 21:11:33  24.05.20240423.572af61  6.8.6-zen1                          *
    57          2024-05-04 09:46:55  24.05.20240423.572af61  6.8.6-zen1                          *
    56          2024-05-04 09:31:39  24.05.20240423.572af61  6.8.6-zen1                          *
    55          2024-05-04 09:23:31  24.05.20240423.572af61  6.8.6-zen1                          *
    54          2024-05-04 09:22:51  24.05.20240423.572af61  6.8.6-zen1                          *
    53          2024-05-04 09:22:04  24.05.20240423.572af61  6.8.6-zen1                          *
    52          2024-05-02 23:30:38  24.05.20240423.572af61  6.8.6-zen1                          *
    51          2024-05-01 18:45:36  24.05.20240423.572af61  6.8.6-zen1                          *
    50          2024-05-01 18:41:19  24.05.20240423.572af61  6.8.6-zen1                          *
    49          2024-05-01 18:30:15  24.05.20240423.572af61  6.8.6-zen1                          *
    48          2024-05-01 17:23:19  24.05.20240423.572af61  6.8.6-zen1                          *
    47          2024-05-01 17:15:15  24.05.20240423.572af61  6.8.6-zen1                          *
    46          2024-05-01 17:14:25  24.05.20240423.572af61  6.8.6-zen1                          *
    45          2024-05-01 16:08:53  24.05.20240423.572af61  6.8.6-zen1                          *
    44          2024-05-01 15:57:55  24.05.20240423.572af61  6.8.6-zen1                          *
    43          2024-05-01 15:54:43  24.05.20240423.572af61  6.8.6-zen1                          *
    42          2024-05-01 15:49:17  24.05.20240423.572af61  6.8.6-zen1                          *
    41          2024-04-30 20:03:42  24.05.20240423.572af61  6.8.6-zen1                          *
    40          2024-04-30 19:55:02  24.05.20240423.572af61  6.8.6-zen1                          *
    39          2024-04-30 19:05:14  24.05.20240423.572af61  6.8.6-zen1                          *
    38          2024-04-30 19:02:55  24.05.20240423.572af61  6.8.6-zen1                          *
    37          2024-04-30 19:02:00  24.05.20240423.572af61  6.8.6-zen1                          *
    36          2024-04-30 18:58:45  24.05.20240423.572af61  6.8.6-zen1                          *
    35          2024-04-30 18:57:41  24.05.20240423.572af61  6.8.6-zen1                          *
    34          2024-04-30 18:52:52  24.05.20240423.572af61  6.8.6-zen1                          *
    33          2024-04-30 18:37:20  24.05.20240423.572af61  6.8.6-zen1                          *
    32          2024-04-30 18:36:39  24.05.20240423.572af61  6.8.6-zen1                          *
    31          2024-04-30 18:31:47  24.05.20240423.572af61  6.8.6-zen1                          *
    30          2024-04-30 18:30:55  24.05.20240423.572af61  6.8.6-zen1                          *
    29          2024-04-29 03:35:38  24.05.20240423.572af61  6.8.6-zen1                          *
    28          2024-04-29 03:33:03  24.05.20240423.572af61  6.8.6-zen1                          *
    27          2024-04-29 03:17:03  24.05.20240423.572af61  6.8.6-zen1                          *
    
        へ
    （• ˕ •マ
       ~mew.

--- system/default.nix
+++ system/default.nix
@@ -292,8 +292,35 @@
 			oh-my-posh
 			vlc
 		
-		(writeShellScriptBin "say-hello" ''
-                	echo "hello world!"
+		(writeShellScriptBin "nix-edit" ''
+#!/run/current-system/sw/bin/env /run/current-system/sw/bin/bash
+
+TIMESTAMP=$(date --rfc-3339=ns)
+
+cd /copycat
+nvim /copycat/system/default.nix
+
+git add -A
+
+RESULT="fail"
+nh os switch --ask
+[[ $? -eq 0 ]] && RESULT="pass"
+
+GENERATIONS=$(nixos-rebuild --flake /copycat/system#$(hostname) list-generations)
+
+COMMIT_MSG=$(cat <<- EOF
+ copycat@$(hostname) [$RESULT] [$TIMESTAMP]
+
+$GENERATIONS
+
+    へ
+（• ˕ •マ
+   ~mew.
+EOF
+
+)
+git commit -m "$COMMIT_MSG"
+sudo git push
 	        '')
 		];
 
